0dfec2e5ccc59da29906eee44274c3d5bfd8eb92
Olov Johansson <johansson.olov@gmail.com>
2018-09-24 12:32:10 +0200

    Tillsnyggning

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ class App extends Component {
         <div>
           <section className="main-content columns is-fullheight">
             <Sidebar data={menyData(this.state.data)} />
-
+<div>
           {this.state.data.miljöer.map(e => <section><Environment data={e} /></section>)}
-
+</div>
           </section>
         </div>
       </div>
